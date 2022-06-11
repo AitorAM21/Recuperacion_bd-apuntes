49cbf09175e8bbbe68cc2cbb52f78d43292d21c1
AitorAM21 <aitoraresmontes21@gmail.com>
2022-06-11 22:09:11 +0200

    4

--- examen vacio - rehacer/src/movies/movie_video.functions.js
+++ examen vacio - rehacer/src/movies/movie_video.functions.js
@@ -5,12 +5,12 @@ exports.getMovieVideos = async (req, res) => {
     const {body, params, query} = req
     const id = params.id || query.id || body.id
 
-    const movie = {} //TODO: Recoger los datos de la pelicula
+    const movie = await mMovie.findById({id}) //TODO: Recoger los datos de la pelicula
     if (!movie) {
         const obj = { message: 'No existe la pelicula' }
         return res.send(obj)
     }
-    const videos = [] // TODO: Recoger los trailers de las peliculas
+    const videos = await mMovieVideo.find({movie_id:movie.id}) // TODO: Recoger los trailers de las peliculas
     const data = { movie, videos }
 
     // Los datos de la pelicula y sus detalles
@@ -18,6 +18,6 @@ exports.getMovieVideos = async (req, res) => {
 }
 
 exports.getLastTrailers = async (req, res) => {
-    const trailers = [] // TODO: Recoger los ultimos 10 trailers
+    const trailers = await mMovieVideo.find({}).sort({'createdAt':-1}).limit(10)  // TODO: Recoger los ultimos 10 trailers
     res.send({trailers})
 }
